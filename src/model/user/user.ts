2e00e01c4f4ccf41db9e4b386925ecf56c434a3d
JaeHo Song <thruthesky@gmail.com>
2017-06-04 20:26:18 +0800

    update for comments

--- src/model/user/user.ts
+++ src/model/user/user.ts
@@ -18,11 +18,19 @@ export interface USER_REGISTER_RESONSE {
 export class User {
     auth;
     constructor( private angularFireAuth: AngularFireAuth ) {
+
+        /**
+         * angularFireAuth.auth 는 Firebase SDK 의 auth 와 같은 것이다.
+         * 
+         * Firebase SDK 의 auth 를 사용하면, 초기화가 되지 않았다고 뭐라고하는데, 아래와 같이 하면, 초기화 되지 않아도 (부모 프로젝트에서 나중에 초기화 하므로) 사용가능하다.
+         */
         this.auth = angularFireAuth.auth;
     }
-
     register( data: USER_REGISTER ) : Observable<firebase.User> {
         let promise = this.auth.createUserWithEmailAndPassword( data.email, data.password );
+        /**
+         * Firebase SDK 의 Promise 를 받아서 Observable 로 리턴한다.
+         */
         return Observable.fromPromise( promise );
     }
 
