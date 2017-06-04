a50e5a87026c0175bce3a97e7bce6b4cb02fcf8b
JaeHo Song <thruthesky@gmail.com>
2017-06-04 20:21:51 +0800

    update for user.

--- src/model/user/user.ts
+++ src/model/user/user.ts
@@ -1,12 +1,9 @@
 import { Injectable } from '@angular/core';
 import { AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireAuth } from 'angularfire2/auth';
 import * as firebase from 'firebase/app';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/fromPromise';
-
-
-
-
 
 export interface USER_REGISTER {
     name: string;
@@ -15,26 +12,16 @@ export interface USER_REGISTER {
 }
 
 export interface USER_REGISTER_RESONSE {
-
 }
 
 @Injectable()
 export class User {
-
     auth;
-
-
-    constructor( ) {
-        
-        this.auth = firebase.auth();
-
-
-
-        
+    constructor( private angularFireAuth: AngularFireAuth ) {
+        this.auth = angularFireAuth.auth;
     }
 
-
-    register( data: USER_REGISTER ) {
+    register( data: USER_REGISTER ) : Observable<firebase.User> {
         let promise = this.auth.createUserWithEmailAndPassword( data.email, data.password );
         return Observable.fromPromise( promise );
     }
