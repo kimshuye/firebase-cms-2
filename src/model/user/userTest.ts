b00624669f9a6d47ed711c5f26ed5bb3f4c6046f
JaeHo Song <thruthesky@gmail.com>
2017-06-07 22:55:55 +0800

    update for thenable

--- src/model/user/userTest.ts
+++ src/model/user/userTest.ts
@@ -23,9 +23,15 @@ export class UserTest {
         let name = 'user' + this.d.getDate() + this.d.getHours() + this.d.getMinutes() + this.d.getSeconds();
         let email = name + '@gmail.com';
         let data: USER_REGISTER = { email: email, password: '12345a', displayName: name, photoUrl: '' };
+
+
+
         this.user.register( data, () => {
             console.log("displayName: ", this.user.auth.currentUser.displayName);
-        }, e => console.error(e) );
+        },
+        e => {
+            console.error(e);
+        });
 
     }
 }
