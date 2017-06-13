e5b5f939773a4a01cd9c70df12f3679076931452
JaeHo Song <thruthesky@gmail.com>
2017-06-13 11:51:38 +0800

    up

--- src/model/forum/forum.interface.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CATEGORY_PATH = 'forum/category';
-exports.POST_DATA_PATH = 'forum/post/data';
-exports.CATEGORY_POST_RELATION_PATH = 'forum/category-post-relation';
-;
-//# sourceMappingURL=forum.interface.js.map
